89617e17fcf86eb98475edaf7a9587fbbec86be3
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2020-09-13 23:08:13 -0300

    Setup github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  router: {
+    base: '/own_portfolio/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
