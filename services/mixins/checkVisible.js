f1ba335f41d6bf0eeb6decb9d58f84d117fdbac5
rdiazutn <rodrigodiaz@est.frba.utn.edu.ar>
2020-09-16 11:44:24 -0300

    Merge branch 'develop'
    # Conflicts:
    #	assets/general.scss
    #	components/Index/Experience/ExperienceDescription.vue
    #	components/Index/HomeSection/HomeSection.vue
    #	components/layout/TheAppBar.vue
    #	services/mixins/checkVisible.js

--- services/mixins/checkVisible.js
+++ services/mixins/checkVisible.js
@@ -3,12 +3,34 @@ export default {
   methods: {
     isElementVisible (id, threshold = 0, mode = 'visible') {
       const elm = document.getElementById(id)
+      if (!elm) {
+        return
+      }
+
       const rect = elm.getBoundingClientRect()
       const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight)
       const above = rect.bottom - threshold < 0
       const below = rect.top - viewHeight + threshold >= 0
 
       return mode === 'above' ? above : (mode === 'below' ? below : !above && !below)
+    },
+    percentageVisible (id) {
+      const elm = document.getElementById(id)
+      if (!elm) {
+        return
+      }
+      // Get the relevant measurements and positions
+      const viewportHeight = window.innerHeight
+      const scrollTop = window.scrollY
+      const elementOffsetTop = elm.offsetTop
+      const elementHeight = elm.offsetHeight
+
+      // Calculate percentage of the element that's been seen
+      const distance = scrollTop + viewportHeight - elementOffsetTop
+      const percentage = Math.round(
+        distance / ((viewportHeight + elementHeight) / 100)
+      )
+      return Math.min(100, Math.max(0, percentage))
     }
   }
 }
